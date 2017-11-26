94b8a257c686385efa574e74e29e7ae8fa07a84d
lrb <1246531124@qq.com>
2017-11-26 19:47:15 +0800

    +

--- src/test/java/connectionPool/TestSimplePool.java
+++ src/test/java/connectionPool/TestSimplePool.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class TestSimplePool {
     private int poolSize = 100;
-    private int clientSize = 1000;
+    private int clientNum = 1000;
 
     private IConnectionPool connectionPool = new SimpleConnectionPool(poolSize);
     private ConnectionProvider provider = new ConnectionProvider();
@@ -17,13 +17,13 @@ public class TestSimplePool {
     @Test
     public void basicTest(){
 
-        Client[] clients = new Client[clientSize];
+        Client[] clients = new Client[clientNum];
         long beginTime = System.currentTimeMillis();
-        for(int i=0;i<clientSize;i++){
+        for(int i=0;i<clientNum;i++){
             clients[i]  = new Client(connectionPool);
             clients[i] .start();
         }
-        for(int i=0;i<clientSize;i++){
+        for(int i=0;i<clientNum;i++){
             try {
                 clients[i].join();
             }catch (InterruptedException e){
@@ -41,12 +41,12 @@ public class TestSimplePool {
     @Test
     public void basicTestWithoutPool(){
         long beginTime = System.currentTimeMillis();
-        Client[] clients1 = new Client[clientSize];
-        for(int i=0;i<clientSize;i++){
+        Client[] clients1 = new Client[clientNum];
+        for(int i=0;i<clientNum;i++){
             clients1[i]  = new Client(provider);
             clients1[i] .start();
         }
-        for(int i=0;i<clientSize;i++){
+        for(int i=0;i<clientNum;i++){
             try {
                 clients1[i].join();
             }catch (InterruptedException e){
@@ -54,7 +54,7 @@ public class TestSimplePool {
             }
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("【未】使用连接池总耗时："+(endTime-beginTime)+"ms");
+        System.out.println("【未使用】连接池总耗时："+(endTime-beginTime)+"ms");
     }
 
 
